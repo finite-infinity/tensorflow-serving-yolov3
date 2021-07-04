27133567a5866ac1b3b768ab16591c8878ca2684
finite-infinity <50870852+finite-infinity@users.noreply.github.com>
2021-07-04 22:34:02 +0800

    Update config.py

--- core/config.py
+++ core/config.py
@@ -38,8 +38,8 @@ __C.TRAIN.DATA_AUG              = True
 __C.TRAIN.LEARN_RATE_INIT       = 1e-4
 __C.TRAIN.LEARN_RATE_END        = 1e-6
 __C.TRAIN.WARMUP_EPOCHS         = 2
-__C.TRAIN.FISRT_STAGE_EPOCHS    = 10
-__C.TRAIN.SECOND_STAGE_EPOCHS   = 30
+__C.TRAIN.FISRT_STAGE_EPOCHS    = 5    #10
+__C.TRAIN.SECOND_STAGE_EPOCHS   = 10   #30
 __C.TRAIN.INITIAL_WEIGHT        = "./checkpoint/yolov3_coco_demo.ckpt"
 
 
